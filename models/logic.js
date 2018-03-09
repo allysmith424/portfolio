13120b2db37aa44c3d6fc9c60bc221c0e1e78b0e
Ally Smith <allysmith@Allys-MacBook-Air.local>
2018-03-09 13:23:22 -0600

    second page slides function, grid of work

--- models/logic.js
+++ models/logic.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage();
+
+	console.log("Hello Ally");
+
+	$('#fullpage').fullpage({
+		sectionsColor: ['#ff6100', '#fff', '#0e2c3a', '#ff6100'],
+		anchors: ['firstPage', 'secondPage', 'thirdPage', 'lastPage'],
+		menu: '#menu',
+		scrollingSpeed: 1000
+	});
+
+	var $grid = $('.grid').imagesLoaded( function() {
+
+		$('.grid').isotope({
+		  itemSelector: '.grid-item',
+		  percentPosition: true,
+		  masonry: {
+		    columnWidth: '.grid-sizer'
+		  }
+		});
+
+	});
+
 });
